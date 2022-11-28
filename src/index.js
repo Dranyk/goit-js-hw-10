8c8b767c7a10e783407c56bd4ad6e543ff3ab64a
Dranyk <dvdranyk@gmail.com>
2022-11-28 20:03:46 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,95 @@
 import './css/styles.css';
 
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const searchBox = document.querySelector('#search-box');
+const counrtyList = document.querySelector('.country-list');
+const counrtyInfo = document.querySelector('.country-info');
+
+
 const DEBOUNCE_DELAY = 300;
+
+const parseResponse = res => {
+  return res.reduce((acc, country) => {
+    delete country.altSpellings;
+    acc.push({
+      ...country,
+      name: country.name.common,
+      flags: country.flags.svg,
+      languages: Object.values(country.languages).join(', '),
+    });
+    return acc;
+  }, []);
+};
+
+const onKeyDown = () => {
+  clearMarkup();
+  const inputFieldvalue = searchBox.value.trim();
+  if (!inputFieldvalue) return;
+  fetchCountries(inputFieldvalue)
+    .then(parseResponse)
+    .then(countries => {
+      if (countries.length > 10) {
+        throw new Error(
+          JSON.stringify({
+            type: 'info',
+            message:
+              'Too many matches found. Please enter a more specific name.',
+          })
+        );
+      }
+      return countries;
+    })
+    .then(countries => {
+      if (countries.length === 1) {
+        counrtyInfo.innerHTML = oneCounrtyMarkup(countries[0]);
+      } else {
+        counrtyList.innerHTML = manyCounrtisListMarkup(countries);
+      }
+    })
+    .catch(err => {
+      const { type, message } = JSON.parse(err.message);
+      Notify[type](message, {
+        fontSize: '16px',
+      });
+    });
+};
+
+searchBox.addEventListener('input', debounce(onKeyDown, DEBOUNCE_DELAY));
+
+
+
+
+const manyCounrtisListMarkup = countries =>
+  countries
+    .map(({ name, flags }) => {
+      return `    
+    <li>
+      <img width="20" height="20"
+      src="${flags}">
+      </img>
+      <p>
+      ${name}</p>
+    </li>`;
+    })
+    .join('');
+
+    const oneCounrtyMarkup = ({
+  name,
+  flags,
+  capital,
+  population,
+  languages,
+}) => `<h2><img width="20" height="20"
+      src="${flags}">
+      </img>${name}</h2>
+    <p><strong>Capital:</strong> ${capital}</p>
+    <p><strong>Population:</strong> ${population}</p>
+    <p><strong>Languages:</strong> ${languages}</p>`;
+
+ const clearMarkup = () => {
+  counrtyList.innerHTML = '';
+  counrtyInfo.innerHTML = '';
+};
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,14 @@
+
+const URL = 'https://restcountries.com/v3.1/name/';
+const FIELDS = '?fields=name,capital,population,flags,languages';
+export const fetchCountries = name =>
+  fetch(URL + name + FIELDS).then(res => {
+    if (!res.ok)
+      throw new Error(
+        JSON.stringify({
+          type: 'failure',
+          message: 'Oops, there is no country with that name',
+        })
+      );
+    return res.json();
+  });
